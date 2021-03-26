2eaf0e4606af6396081e9f61c18ac496e7f2f002
Mara Dusevic <s200494@students.aie.edu.au>
2021-03-26 16:45:08 +1100

    Updated animation, added character controller

---
+++ StandardPipeline/ShaderGraph_URP/Assets/Scripts/SimplePlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("IsAiming", true);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("IsAiming", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("IsJumping", false);
+        }
+    }
+}
