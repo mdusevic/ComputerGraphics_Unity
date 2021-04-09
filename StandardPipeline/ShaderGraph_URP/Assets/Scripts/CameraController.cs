8c2c4fec89fb96beba65879ada65cacfc0d0f850
Mara Dusevic <s200494@students.aie.edu.au>
2021-04-09 16:46:15 +1000

    Added new animations, character, fixed player movement

---
+++ StandardPipeline/ShaderGraph_URP/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 1000.0f;
+
+    public GameObject target;
+
+    public float minTurnAngle = -90.0f;
+    public float maxTurnAngle = 0.0f;
+    private float rotX;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Get the mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        rotX -= mouseY;
+        rotX = Mathf.Clamp(rotX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotX, 0f, 0f);
+        target.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ StandardPipeline/ShaderGraph_URP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+    Rigidbody rb;
+
+    public float speed = 18.0f;
+    public float gravity = 8.0f;
+    public float jumpHeight = 10.0f;
+
+    public float groundDistance = 0.4f;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            velocity.y = jumpHeight;
+        }
+
+        velocity.y -= gravity * Time.deltaTime;
+        animator.SetBool("IsGrounded", isGrounded);
+
+        controller.Move(velocity * Time.deltaTime);
+
+        animator.SetFloat("PosX", Input.GetAxis("Horizontal"));
+        animator.SetFloat("PosY", Input.GetAxis("Vertical"));
+    }
+}
